4e32613ba2b439318c00990ff0d64e8f96a3e1c4
chrizmoko <christianmok2000@yahoo.com>
2020-09-10 18:49:19 -0700

    Minor changes to random AI.

--- src/connectfour/ai/RandomAI.java
+++ src/connectfour/ai/RandomAI.java
@@ -1,5 +1,6 @@
 package connectfour.ai;
 
+import connectfour.ai.util.AI;
 import connectfour.core.GameState;
 
 public class RandomAI extends AI {
@@ -7,7 +8,7 @@ public class RandomAI extends AI {
 		super();
 	}
 	
-	public int chooseMove(GameState state) {
+	public int chooseMove(final GameState state) {
 		return (int)(Math.random() * state.getBoard().getRows());
 	}
 }
--- src/connectfour/ai/RandomAISupplier.java
+++ src/connectfour/ai/RandomAISupplier.java
@@ -1,7 +1,14 @@
 package connectfour.ai;
 
+import connectfour.ai.util.AI;
+import connectfour.ai.util.AISupplier;
+
 public class RandomAISupplier implements AISupplier {
 	public AI get() {
 		return new RandomAI();
+	}
+	
+	public String getAIName() {
+		return "Random Column AI";
 	}
 }
