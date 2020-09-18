3cf2d22f54522ed198ecfc24fdf0a7b1fbb70081
chrizmoko <christianmok2000@yahoo.com>
2020-09-18 02:24:47 -0700

    Removed constructor since it is superfluous to the default constructor in the abstract class.

--- src/connectfour/ai/OneColumnAI.java
+++ src/connectfour/ai/OneColumnAI.java
@@ -4,10 +4,6 @@ import connectfour.ai.util.AbstractAI;
 import connectfour.core.GameState;
 
 public class OneColumnAI extends AbstractAI {
-	public OneColumnAI() {
-		super();
-	}
-	
 	public int chooseMove(GameState state) {
 		return 0;
 	}
--- src/connectfour/ai/RandomAI.java
+++ src/connectfour/ai/RandomAI.java
@@ -4,10 +4,6 @@ import connectfour.ai.util.AbstractAI;
 import connectfour.core.GameState;
 
 public class RandomAI extends AbstractAI {
-	public RandomAI() {
-		super();
-	}
-	
 	public int chooseMove(final GameState state) {
 		return (int)(Math.random() * state.getBoard().getRows());
 	}
