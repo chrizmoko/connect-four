539a6203c210fd6c534dd89813d58f9a96410d13
chrizmoko <crmok@uci.edu>
2020-12-31 01:09:40 -0800

    Exceptions added to cover null value cases.

--- src/connectfour/ai/util/AIFactory.java
+++ src/connectfour/ai/util/AIFactory.java
@@ -6,15 +6,24 @@ public class AIFactory {
 	private static final AbstractMap<String, AISupplier> suppliers = new HashMap<>();
 	
 	public static void registerSupplier(String name, AISupplier supplier) throws AIFactoryException {
+		if (name == null) {
+			throw new AIFactoryException("A supplier cannot have a name that is a null value.");
+		}
+		if (supplier == null) {
+			throw new AIFactoryException("A supplier cannot be a null value.");
+		}
 		if (suppliers.containsKey(name)) {
-			throw new AIFactoryException("A supplier under the name " + name + " has already been registered.");
+			throw new AIFactoryException("A supplier under the name \"" + name + "\" has already been registered.");
 		}
 		suppliers.put(name, supplier);
 	}
 	
 	public static void unregisterSupplier(String name) throws AIFactoryException {
+		if (name == null) {
+			throw new AIFactoryException("A supplier cannot have a name that is a null value.");
+		}
 		if (!suppliers.containsKey(name)) {
-			throw new AIFactoryException("A supplier under the name " + name + " does not exist.");
+			throw new AIFactoryException("A supplier under the name \"" + name + "\" does not exist.");
 		}
 		suppliers.remove(name);
 	}
