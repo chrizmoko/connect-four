a50bcbb34d930162aaa86838c1f6fab1bb00c4a8
chrizmoko <crmok@uci.edu>
2021-01-16 03:22:05 -0800

    Framework of the min-max algorithm. However, a good heuristic/evaluate state function would needs to be created.

---
+++ src/connectfour/ai/MinMaxAI.java
@@ -0,0 +1,107 @@
+package connectfour.ai;
+
+import connectfour.ai.util.AbstractAI;
+import connectfour.core.GameState;
+import connectfour.core.Board;
+import connectfour.core.Cell;
+import connectfour.core.ConnectFourException;
+
+public class MinMaxAI extends AbstractAI {
+	private int depth;
+	
+	public MinMaxAI(int depth) {
+		if (depth < 0) {
+			throw new IllegalArgumentException("Depth cannot be a negative number.");
+		}
+		this.depth = depth;
+	}
+	
+	@Override
+	public int chooseMove(GameState state) {
+		// Initial decision (for comparison) for a valid column
+		GameState copy = state.copy();
+		int decision = 0;
+		while (true) {
+			try {
+				copy.makeMove(decision);
+				break;
+			} catch (ConnectFourException e) {
+				decision++;
+			}
+		}
+		
+		// From the result of the initial comparison value, find the best move
+		int maxValue = minmax(copy, depth, true);
+		for (int i = decision + 1; i < state.getBoard().getNumColumns(); i++) {
+			copy = state.copy();
+			
+			try {
+				copy.makeMove(i);
+			} catch (ConnectFourException e) {
+				continue;
+			}
+			
+			int tempValue = minmax(copy, depth, true);
+			if (tempValue > maxValue) {
+				maxValue = tempValue;
+				decision = i;
+			}
+		}
+		
+		return decision;
+	}
+	
+	private int minmax(GameState state, int depth, boolean maximize) {
+		if (depth == 0 || state.isGameOver()) {
+			// Calculate the heuristic of the current board
+			Cell player = (state.isRedTurn()) ? Cell.RED : Cell.YELLOW;
+			Cell opponent = (state.isRedTurn()) ? Cell.YELLOW : Cell.RED;
+			
+			return heuristic(state.getBoard(), player, opponent);
+		}
+		
+		if (maximize) {
+			// Maximize
+			int maxValue = -1;
+			for (int c = 0; c < state.getBoard().getNumColumns(); c++) {
+				GameState copy = state.copy();
+				
+				try {
+					copy.makeMove(c);
+				} catch (ConnectFourException e) {
+					continue;
+				}
+				
+				int tempValue = minmax(copy, depth - 1, false);
+				if (tempValue > maxValue) {
+					maxValue = tempValue;
+				}
+			}
+			return maxValue;
+		} else {
+			// Minimize
+			int minValue = 1;
+			for (int c = 0; c < state.getBoard().getNumColumns(); c++) {
+				GameState copy = state.copy();
+				
+				try {
+					copy.makeMove(c);
+				} catch (ConnectFourException e) {
+					continue;
+				}
+				
+				int tempValue = minmax(copy, depth - 1, true);
+				if (tempValue < minValue) {
+					minValue = tempValue;
+				}
+			}
+			return minValue;
+		}
+	}
+	
+	private int heuristic(Board board, Cell player, Cell opponent) {
+		// TODO: Figure out a good heuristic to evaluate the connect four board
+		
+		return -1;
+	}
+}
